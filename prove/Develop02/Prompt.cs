6ebb66a9fb703e4c60a9972eb8f4fcefcf03ea57
KyleeCodes <ktawesome1@gmail.com>
2023-12-10 14:42:50 -0700

    Week 2 program complete

---
+++ prove/Develop02/Prompt.cs
@@ -0,0 +1,16 @@
+public static class Prompt {
+    public static string[] _prompts;
+    static Prompt() {
+        _prompts = new string[] {
+            "Did anything exciting happen today?",
+            "What was your favorite thing that happened?",
+            "Was there something you feel you could've done differently today? What would you have done instead?",
+            "Did you do anything to serve others today? How did it make you feel?",
+            "How are you feeling, really?"
+            };
+    }
+
+    public static string obtainPrompt(){
+        return _prompts[new Random().Next(5)];
+    }
+}
